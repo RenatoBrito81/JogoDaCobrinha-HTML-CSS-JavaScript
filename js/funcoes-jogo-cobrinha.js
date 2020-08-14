2b45ccbc3abc182e9d1eaa3ba174bd2943eb4e0f
Renato Brito <rb.renatobrito@gmail.com>
2020-08-14 06:22:24 -0300

    Adionado controle de direcao e movimento.

--- js/funcoes-jogo-cobrinha.js
+++ js/funcoes-jogo-cobrinha.js
@@ -22,8 +22,29 @@ function criarCobrinha(){
     }
 }
 
+//Cria evento para verificar o teclado
+document.addEventListener("keydown", (event) => {
+    //Seta para esquerda
+    if(event.keyCode === 37 && direcao !== "direita") direcao = "esquerda";
+
+    //Seta para cima
+    if(event.keyCode === 38 && direcao !== "descer") direcao = "subir";
+
+    //Seta para direita
+    if(event.keyCode === 39 && direcao !== "esquerda") direcao = "direita";
+
+    //seta para baixo
+    if(event.keyCode === 40 && direcao !== "subir") direcao = "descer";
+});
+
 //Função para movimentar a cobrinha
 function movimentarCobrinha(){
+    //Verifica a posição da cabeça e a direção escolhida
+    if(cobrinha[0].eixoX > (15 * caixa) && direcao === "direita") cobrinha[0].eixoX = 0;
+    if(cobrinha[0].eixoX < 0 && direcao === "esquerda") cobrinha[0].eixoX = 15 * caixa;
+    if(cobrinha[0].eixoY > (15 * caixa) && direcao === "descer") cobrinha[0].eixoY = 0;
+    if(cobrinha[0].eixoY < 0 && direcao === "subir") cobrinha[0].eixoY = 15 * caixa;
+
     //Cria área do jogo
     criarAreaJogo();
 
