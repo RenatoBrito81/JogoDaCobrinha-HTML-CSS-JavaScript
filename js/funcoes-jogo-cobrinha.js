1a8baaee9bfb4c64679483c6bdbc8f3c667ad30b
Renato Brito <rb.renatobrito@gmail.com>
2020-08-15 07:35:59 -0300

    Criado novo layout com novas opcoes para o jogo.

--- js/funcoes-jogo-cobrinha.js
+++ js/funcoes-jogo-cobrinha.js
@@ -13,11 +13,22 @@ var fruta = {
 
 var direcao = "direita";
 
+ranking = [];
+
+/*var ranking = [
+    {pontos: 10, velocidade: 10, borda: false},
+    {pontos: 7, velocidade: 8, borda: true},
+    {pontos: 5, velocidade: 5, borda: true},
+    {pontos: 2, velocidade: 2, borda: true},
+    {pontos: 1, velocidade: 1, borda: false}
+];*/
+
 //Função para criar a área do jogo
 function criarAreaJogo(){
     contexto.fillStyle = "lightgreen";
     contexto.fillRect(0, 0, (16 * caixa), (16 * caixa));
 }
+criarAreaJogo();
 
 //Função para criar a cobrinha
 function criarCobrinha(){
@@ -47,6 +58,30 @@ document.addEventListener("keydown", (event) => {
 
     //seta para baixo
     if(event.keyCode === 40 && direcao !== "subir") direcao = "descer";
+
+    //*** excluir daqui para baixo (testes)
+    //console.log(event.keyCode);
+    if(event.keyCode === 65){
+        //a
+        document.getElementById("borda-infinita").disabled = true;
+    }
+    if(event.keyCode === 66){
+        //b
+        document.getElementById("borda-infinita").disabled = false;
+    }
+    if(event.keyCode === 67){
+        //c
+        var aux = document.getElementById("lista-velocidades");
+        alert(aux.value);
+    }
+    if(event.keyCode === 68){
+        //d
+        //
+    }
+    if(event.keyCode === 69){
+        //e
+        //
+    }
 });
 
 //Função para movimentar a cobrinha
@@ -71,13 +106,14 @@ function movimentarCobrinha(){
     //Cria cobrinha na área do jogo
     criarCobrinha();
 
+    //Cria frutinha na área do jogo
     criarFrutinha();
 
-    //***
+    //Armazena a posição da cabeça da cobrinha
     let posicaoEixoX = cobrinha[0].eixoX;
     let posicaoEixoY = cobrinha[0].eixoY;
 
-    //***
+    //Verifica a direção da cobrinha
     if(direcao == "direita") posicaoEixoX += caixa;
     if(direcao == "esquerda") posicaoEixoX -= caixa;
     if(direcao == "subir") posicaoEixoY -= caixa;
@@ -89,10 +125,10 @@ function movimentarCobrinha(){
         cobrinha.pop();
     }
     else{
+        //Define uma nova posição para a frutinha
         fruta.eixoX = Math.floor(Math.random() * 15 + 1) * caixa;
         fruta.eixoY = Math.floor(Math.random() * 15 + 1) * caixa;
     }
-    
 
     //Incluí nova posição no inicio do array da cobrinha
     cobrinha.unshift({
@@ -102,4 +138,42 @@ function movimentarCobrinha(){
 }
 
 //Função para fazer os movimentos da cobrinha
-var movimentar = setInterval(movimentarCobrinha, 100);
+var movimentar = setInterval(movimentarCobrinha, 500);
+
+//Função para resetar o ranking
+function resetarRanking(){
+    //Referência a tabela do ranking
+    let tab = document.getElementById("tabela-ranking");
+    
+    //Exclui as linhas enquanto houver linhas além do cabeçalho
+    while(tab.rows.length>1){
+        tab.deleteRow(tab.rows.length-1);
+    }
+}
+
+//Função para atualizar a tabela do ranking
+function atualizarRanking(){
+    //Referência a tabela do ranking
+    let tab = document.getElementById("tabela-ranking");
+
+    //Reseta o ranking
+    resetarRanking();
+
+    //Itera no array com o ranking
+    for(const rank of ranking){
+        console.log(rank.pontos, rank.velocidade, rank.borda);
+        var novaLinha = tab.insertRow(tab.rows.length);
+        
+        var celulaPosicao = novaLinha.insertCell(0);
+        celulaPosicao.innerHTML = tab.rows.length-1 + "°";
+
+        var celulaPontos = novaLinha.insertCell(1);
+        celulaPontos.innerHTML = rank.pontos;
+
+        var celulaVelocidade = novaLinha.insertCell(2);
+        celulaVelocidade.innerHTML = rank.velocidade;
+
+        var celulaBorda = novaLinha.insertCell(3);
+        celulaBorda.innerHTML = (rank.borda) ? "Ativada" : "Desativada";
+    }
+}
